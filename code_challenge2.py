1dcff4df75f7942dbc4be6379c2eeba99b061168
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-08-15 12:56:23 +0800

    code_challenge2

---
+++ code_challenge2.py
@@ -0,0 +1,41 @@
+deposit = eval(input(" Enter amount to deposit -->",))
+print("	Here is breakdown of the deposit amount:")
+
+thousand = deposit // 1000
+deposit = deposit % 1000
+
+fivehundred = deposit // 500
+deposit = deposit % 500
+
+twohundred = deposit // 200
+deposit = deposit % 200
+
+onehundred = deposit // 100
+deposit = deposit % 100
+
+fifty = deposit // 50
+deposit = deposit % 50
+
+twenty = deposit // 20
+deposit = deposit % 20
+
+ten = deposit // 10
+deposit = deposit % 10
+
+five = deposit // 5
+deposit = deposit % 5
+
+one = deposit // 1
+deposit = deposit % 1
+
+
+
+print(thousand,"-1000")
+print(fivehundred,"-500")
+print(twohundred,"-200")
+print(onehundred,"-100")
+print(fifty,"-50")
+print(twenty,"-20")
+print(ten,"-10")
+print(five,"-5")
+print(one,"-1")
