db2652fdcf8bc3518bdc2c05402521c85b132606
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-08-18 15:50:48 +0800

    Activity9.py

---
+++ activity9.py
@@ -0,0 +1,13 @@
+a = 27
+b = 72
+
+c = 21
+d = 10
+email = 'princess'
+password = 'supercute'
+print((email == 'princess' and password == 'supercute'))
+
+#print ( (a > b) and (c < d) )
+#print( (a > b) or (c < d ) )
+#print(not(a > b) or (C < d) )
+
