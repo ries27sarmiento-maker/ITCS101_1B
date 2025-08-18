a8cabc6bbf3e2c80ad75d2c55838f9d6d4ffa6fe
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-08-18 15:41:20 +0800

    Ativity7.py

---
+++ activity7.py
@@ -0,0 +1,19 @@
+a = 5
+
+print("The value of a is", a)
+
+a += 72
+a -= 27
+a *= 21
+a -= 14
+a += 10 
+a += 72
+
+print("The value of a is", a)
+
+b = 12
+b += 32
+b -= 53
+b *= 3
+
+print("The value of a is", b)
