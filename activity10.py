351980c3dce11bee270d4c07f067028bc256b133
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-08-27 15:05:50 +0800

    Activity10.py

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "princess"
+password = "cutemo123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+        print("ACCESS GRANTED")
+else:	
+	print("ACCESS DENIED")
