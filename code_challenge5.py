096bd7d3aac28df46b88b59b37bb81ded3afa431
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-15 15:47:51 +0800

    Create code_challenge5.py

---
+++ code_challenge5.py
@@ -0,0 +1,10 @@
+print("The Factorial program")
+
+number = eval(input ("Enter the number:"))
+y = 1
+
+for x in range(number, 0, -1):
+  y *= x
+print("The factorial",number,"is",y)
+
+            
