d25c6583ab3c260e16fb0696e3c4e70d76508094
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-15 15:29:25 +0800

    Create Activitiy11.py

---
+++ Activitiy11.py
@@ -0,0 +1,16 @@
+temp = eval(input("Enter the Temperature here -->"))
+
+if temp <= 0:
+ print("Let it go - FROZEN temperature ")
+if temp >= 1 and temp <= 15:
+ print("Extremely cold"
+if temp >= 16 and temp <= 38:
+ print("Cold Temperature")
+if temp >= 39 and temp 44:
+ print("Warm Temperature")
+if temp >= 45 and temp <= 50:
+ print("Hot temperature - Inom kana gamot")
+if temp >= 51 and temp <= 60:
+ print("Extremely hot Temperature ")
+if temp >= 61:
+ print("Burning Temperature ")
