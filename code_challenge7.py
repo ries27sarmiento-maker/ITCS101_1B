63c9e0dd34eea6879d8aaa8fca9dc1669cc3d0e8
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-17 13:28:25 +0800

    Add files via upload

---
+++ code_challenge7.py
@@ -0,0 +1,9 @@
+print("The Countdown Timer Simulator!")
+
+start_number = int(input("Enter the starting number for countdown:"))
+
+print("The countdown start now!")
+
+for countdown in range(start_number,0 ,-1):
+	print(countdown)
+print("Liftoff!")
