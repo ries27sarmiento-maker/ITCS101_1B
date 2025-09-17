1f15c2a70a8617301d66cb78506ada4947c314e4
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-17 18:54:16 +0800

    Delete code_challenge9.py

--- code_challenge9.py
+++
@@ -1,9 +0,0 @@
-print("PARROT SIMULATOR - THE ECHO CHAMBER!")
-
-parrot = input("What do you want your parrot say? -->")
-
-times = int(input("How many times should the parrot say squackk?-->"))
-
-print("🗣️LISTEN TO YOUR PARROT ")
-for times in range(times):
-	print(f"🦜SQUACKKK! {parrot}")
