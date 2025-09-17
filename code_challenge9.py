68cec2792975031be38d19edb4d3fe0f0d99d188
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-17 18:55:52 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,9 @@
+print("PARROT SIMULATOR - THE ECHO CHAMBER!")
+
+parrot = input("What do you want your parrot say? -->")
+
+times = int(input("How many times do you want your parrot say? -->"))
+
+print("🗣️LISTEN TO YOUR PARROT ")
+for times in range(times):
+	print(f"🦜SQUAWCKKK! {parrot}!")
