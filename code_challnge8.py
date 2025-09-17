eb8ac7b1d89732573df52470edd5c07cf1c80897
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-17 13:30:06 +0800

    Delete code_challnge8.py

--- code_challnge8.py
+++
@@ -1,8 +0,0 @@
-print("Multiplication Table Maker")
-
-number = int(input("Enter a number -->"))
-print("Multiplication Table for(number):")
-for i in range(1, 11):
-	z = number * i
-	print(number, "x", i," =",z)
-#print{f} = (numver) {x} = {z}
