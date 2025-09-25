a1bc359ce8b37fe7dd7ec131cf07a5513dafee9a
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-25 10:06:15 +0800

    Add files via upload

---
+++ Activity15.py
@@ -0,0 +1,13 @@
+fname = 'Princess'
+mname = 'Sarmiento'
+suname = 'Timtim'
+nname = '(rie)'
+
+
+name = input("Do you want to know my name? (yes/no)" ":")
+
+if name == 'yes':
+	print(f"I am {fname} {mname} {suname} {nname}")
+
+if name == 'no':
+	print("okayy :((")
