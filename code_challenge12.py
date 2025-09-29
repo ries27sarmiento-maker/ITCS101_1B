a09164c8f9ec96f062fbbaa7405e3307d73e88b0
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-29 19:46:06 +0800

    Delete code_challenge12.py

--- code_challenge12.py
+++
@@ -1,8 +0,0 @@
-for i in range(1, 10, 1):
-	for z in range(11, i, -1):
-		print(" ", end = " ")
-	for p in range(i, 0, -1):
-		print(p, end = " ")
-	for j in range(1, i + 1, 1):
-		print(j, end = " ")
-	print()
