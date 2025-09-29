e16423b3602ffae9da891515f4a8dd3b9b55196b
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-09-29 18:51:36 +0800

    Add files via upload

---
+++ Activity 20.py
@@ -0,0 +1,20 @@
+print("\t\t ⭐", end= "  ")
+
+for i in range(1,11,1):
+	for x in range(10, i, -1):
+		print(" ", end= " ")
+	for z in range(1, i, 1):
+		print("*", end= " ")
+	for p in range(1, i, 1):
+		print("*", end= " ")
+	print()
+
+
+for i in range(1,11,1):
+	for x in range(1, i, 1):
+		print(" ", end= " ")
+	for z in range(10, i, -1):
+		print("*", end= " ")
+	for p in range(10, i, -1):
+		print("*", end= " ")
+	print()
