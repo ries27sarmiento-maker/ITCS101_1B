b16e45cdfd99c839d6c514d094306357303bcc4e
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-11-19 14:45:17 +0800

    Add files via upload

---
+++ Activity23.py
@@ -0,0 +1,14 @@
+brands = ['apple', 'vivo', 'infinix', 'tecno']
+
+print(brands[3])
+print(brands [3:5])
+print(brands)
+
+brands.append("oppo")
+print(brands)
+
+brands.append("huawei")
+print(brands)
+
+brands. insert(3,"samsung")
+print(brands)
