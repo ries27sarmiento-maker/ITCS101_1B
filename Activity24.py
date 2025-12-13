f2bfab4318c441b8f3849a11d7b3b0b728cf2b15
ries27sarmiento-maker <ries27sarmiento@gmail.com>
2025-12-13 11:52:31 +0800

    Add files via upload

---
+++ Activity24.py
@@ -0,0 +1,12 @@
+from activity24.1 import rieCUTE, cessBALIW
+
+
+def rieCUTE():
+    print("Hi,Rie ! How are you?")
+    print("Hope you are okay")
+
+rieCUTE()
+rieCUTE("Timtim")
+rieCUTE("Perez")
+cessBALIW(input("Your name "), input("Your location "), input("Your age "))
+
